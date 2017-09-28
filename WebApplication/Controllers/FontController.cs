509ed171c6127a77133fd037322efaccbe3c666d
Kiyotaka Abe <k-abe@s-giken.co.jp>
2017-09-28 11:30:28 +0900

    u

--- WebApplication/Controllers/FontController.cs
+++ WebApplication/Controllers/FontController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +10,13 @@ namespace WebApplication.Controllers
     public class FontController : Controller
     {
         // GET: Font
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
+			Debug.WriteLine($"fontName={id}");
+			ViewBag.FontName = id;
             return View();
         }
+
+
     }
 }
